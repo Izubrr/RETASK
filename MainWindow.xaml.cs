a956089c068347ced0e5f4f6e02ff3078d1834e6
Izubrr <noma.mici@gmail.com>
2023-04-03 23:01:26 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace RETASK
+{
+    public partial class MainWindow : Window, IComponentConnector
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Mon.Fill =  white;
+            Tue.Fill =  white;
+            Wed.Fill =  white;
+            Thu.Fill =  white;
+            Fri.Fill =  white;
+            Sat.Fill =  white;
+            Sun.Fill =  white;
+            WorkWithTasks task = new WorkWithTasks();
+            task.updateTasks( HomeContainer);
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += (sender, e) =>
+            {
+                task.updateTasks(HomeContainer);
+            };
+            timer.Interval = TimeSpan.FromHours(1);
+            timer.Start();
+        }
+        Brush
+            orange = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFE66F24"),
+            darkorange = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFC75E1C"),
+            blue = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF424D88"),
+            black = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF000000"),
+            gray = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF3E3E3E"),
+            lightgray = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFABADB3"),
+            ultralightgray = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFFFFFFF"),
+            white = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFFFFFFF"),
+            transparent = (SolidColorBrush)new BrushConverter().ConvertFrom("#00FFFFFF"),
+            hoverblue = (SolidColorBrush)new BrushConverter().ConvertFrom("#7eb4ea");
+
+        private void HideContainers()
+        {
+             HomeContainer.Visibility = Visibility.Hidden;
+             AddContainer.Visibility = Visibility.Hidden;
+             StatsContainer.Visibility = Visibility.Hidden;
+             DeleteContainer.Visibility = Visibility.Hidden;
+             HomeContainerScroll.Visibility = Visibility.Hidden;
+             DeleteContainerScroll.Visibility = Visibility.Hidden;
+             TodaySwitchIcon.Visibility = Visibility.Hidden;
+             TodaySwitchButton.Visibility = Visibility.Hidden;
+             HomeButton.Fill = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFE66F24");
+             AddButton.Fill = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFE66F24");
+             StatsButton.Fill = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFE66F24");
+             DeleteButton.Fill = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFE66F24");
+        }
+
+        public void DeleteContainer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WorkWithTasks task = new WorkWithTasks();
+            Canvas clickedObject = e.Source as Canvas;
+            WrapPanel wrapPanel = clickedObject.Parent as WrapPanel;
+            int index = wrapPanel.Children.IndexOf(clickedObject);
+             task.DeleteTask(clickedObject, index);
+        }
+
+        private void TodaySwitch(object sender, MouseButtonEventArgs e)
+        {
+            WorkWithTasks task = new WorkWithTasks();
+            if (TodaySwitchIcon.PrimaryColor == orange)
+            {
+                 TodaySwitchIcon.PrimaryColor =  ultralightgray;
+                 task.LoadTasks( HomeContainer, "Delete");
+            }
+            else
+            {
+                 TodaySwitchIcon.PrimaryColor =  orange;
+                 task.updateTasks( HomeContainer);
+            }
+        }
+
+        private void sidebutton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WorkWithTasks task = new WorkWithTasks();
+            Rectangle btn = (Rectangle)sender;
+            string name = btn.Name;
+
+            switch (name)
+            {
+                case "HomeButton":
+                    HideContainers();
+                    task.updateTasks(HomeContainer);
+                    TodaySwitchIcon.PrimaryColor = orange;
+                    HomeContainer.Visibility = Visibility.Visible;
+                    HomeContainerScroll.Visibility = Visibility.Visible;
+                    TodaySwitchIcon.Visibility = Visibility.Visible;
+                    TodaySwitchButton.Visibility = Visibility.Visible;
+                    HomeButton.Fill = blue;
+                    break;
+                case "AddButton":
+                    HideContainers();
+                    AddContainer.Visibility = Visibility.Visible;
+                    AddButton.Fill = blue;
+                    Description.Text = "";
+                    break;
+                case "DeleteButton":
+                    HideContainers();
+                    DeleteContainer.Visibility = Visibility.Visible;
+                    DeleteContainerScroll.Visibility = Visibility.Visible;
+                    DeleteButton.Fill = blue;
+                    task.LoadTasks(DeleteContainer, "Delete");
+                    break;
+            }
+        }
+
+        private void sidebutton_MouseEnter(object sender, EventArgs e)
+        {
+            Rectangle btn = (Rectangle)sender;
+            string name = btn.Name;
+            switch (name)
+            {
+                case "HomeButton":
+                    if (HomeContainer?.Visibility == Visibility.Hidden) btn.Fill = darkorange;
+                    break;
+                case "AddButton":
+                    if (AddContainer?.Visibility == Visibility.Hidden) btn.Fill = darkorange;
+                    break;
+                case "StatsButton":
+                    if (StatsContainer?.Visibility == Visibility.Hidden) btn.Fill = darkorange;
+                    break;
+                case "DeleteButton":
+                    if (DeleteContainer?.Visibility == Visibility.Hidden) btn.Fill = darkorange;
+                    break;
+            }
+
+        }
+        private void sidebutton_MouseLeave(object sender, EventArgs e)
+        {
+            Rectangle btn = (Rectangle)sender;
+            string name = btn.Name;
+            switch (name)
+            {
+                case "HomeButton":
+                    if (HomeContainer?.Visibility == Visibility.Hidden) btn.Fill = orange;
+                    break;
+                case "AddButton":
+                    if (AddContainer?.Visibility == Visibility.Hidden) btn.Fill = orange;
+                    break;
+                case "StatsButton":
+                    if (StatsContainer?.Visibility == Visibility.Hidden) btn.Fill = orange;
+                    break;
+                case "DeleteButton":
+                    if (DeleteContainer?.Visibility == Visibility.Hidden) btn.Fill = orange;
+                    break;
+            }
+        }
+
+                string selectedcolor = "#FF219040";
+        private Dictionary<string, bool> days = new Dictionary<string, bool>
+        {
+            { "Mon", false },
+            { "Tue", false },
+            { "Wed", false },
+            { "Thu", false },
+            { "Fri", false },
+            { "Sat", false },
+            { "Sun", false }
+        };
+
+        private void SaveTaskButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WorkWithTasks task = new WorkWithTasks();
+            task.AddTask( HomeContainer,  Description.Text,  days,  selectedcolor);
+             HideContainers();
+             HomeContainer.Visibility = Visibility.Visible;
+             HomeContainerScroll.Visibility = Visibility.Visible;
+             HomeButton.Fill =  blue;
+        }
+
+        private void colorbutton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DefaultInterpolatedStringHandler defaultInterpolatedStringHandler = new DefaultInterpolatedStringHandler(0, 1);
+            defaultInterpolatedStringHandler.AppendFormatted<Color>(((SolidColorBrush)((Rectangle)sender).Fill).Color);
+             selectedcolor = defaultInterpolatedStringHandler.ToStringAndClear();
+        }
+
+        private void DoneButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill =  transparent;
+        }
+
+        private void AddContainer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+             TaskExample.Background = (SolidColorBrush)new BrushConverter().ConvertFrom( selectedcolor);
+             NameExample.Content =  Description.Text;
+             DayExample.Content = "";
+            foreach (KeyValuePair<string, bool> day in  days)
+            {
+                bool value = day.Value;
+                if (value)
+                {
+                    Label dayExample =  DayExample;
+                    object content = dayExample.Content;
+                    dayExample.Content = ((content != null) ? content.ToString() : null) + " " + day.Key;
+                }
+            }
+        }
+
+        private void EN_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TEXT1.Content = "Add Tasks";
+            TEXT2.Content = "Achieve your goals systematically";
+            TEXT3.Content = "Name your task";
+            TEXT4.Content = "Weekly frequency";
+            TEXT5.Content = "Mon";
+            TEXT6.Content = "Tue";
+            TEXT7.Content = "Wen";
+            TEXT8.Content = "Thu";
+            TEXT9.Content = "Fri";
+            TEXT10.Content = "Sat";
+            TEXT11.Content = "Sun";
+            TEXT12.Content = "Task color";
+            Settings.Visibility = Visibility.Hidden;
+        }
+
+        private void RU_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TEXT1.Content = "Добавить задачу";
+            TEXT2.Content = "Достигайте своих целей систематично";
+            TEXT3.Content = "Назовите свою задачу";
+            TEXT4.Content = "Еженедельная периодичность";
+            TEXT5.Content = "Пн";
+            TEXT6.Content = "Вт";
+            TEXT7.Content = "Ср";
+            TEXT8.Content = "Чт";
+            TEXT9.Content = "Пт";
+            TEXT10.Content = "Сб";
+            TEXT11.Content = "Вс";
+            TEXT12.Content = "Цвет задачи";
+            Settings.Visibility = Visibility.Hidden;
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            bool flag = e.ChangedButton == MouseButton.Left;
+            if (flag)
+            {
+                base.DragMove();
+            }
+        }
+
+        private void SettingsButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            bool flag =  Settings.Visibility == Visibility.Hidden;
+            if (flag)
+            {
+                 Settings.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                 Settings.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Description_TextChanged(object sender, TextChangedEventArgs e)
+        {
+             NameExample.Content =  Description.Text;
+        }
+
+        private void DoneButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill =  hoverblue;
+        }
+
+        private void day_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle btn = (Rectangle)sender;
+            bool flag = btn.Fill == white;
+            btn.Fill = ultralightgray;
+            string name = btn.Name;
+            if (days.ContainsKey(name))
+            {
+                days[name] = !days[name];
+            }
+        }
+
+        private void buttons_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Stroke =  hoverblue;
+            ((Rectangle)sender).StrokeThickness = 2.5;
+        }
+
+        private void buttons_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Stroke =  lightgray;
+            ((Rectangle)sender).StrokeThickness = 1.0;
+        }
+
+        private void CloseButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            base.Close();
+        }
+
+        private void MinimizeButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            base.WindowState = WindowState.Minimized;
+        }
+
+        private void windowbutton_MouseEnter(object sender, EventArgs e)
+        {
+            Rectangle btn = (Rectangle)sender;
+            string name = btn.Name;
+            string a = name;
+            if (!(a == "CloseButton"))
+            {
+                if (a == "MinimizeButton")
+                {
+                     MinimizeRectangle1.Fill =  black;
+                     MinimizeRectangle1.Height = 2.2;
+                }
+            }
+            else
+            {
+                 CloseRectangle1.Fill =  black;
+                 CloseRectangle2.Fill =  black;
+                 CloseRectangle1.Height = 2.2;
+                 CloseRectangle2.Height = 2.2;
+            }
+        }
+
+        private void windowbutton_MouseLeave(object sender, EventArgs e)
+        {
+            Rectangle btn = (Rectangle)sender;
+            string name = btn.Name;
+            string a = name;
+            if (!(a == "CloseButton"))
+            {
+                if (a == "MinimizeButton")
+                {
+                     MinimizeRectangle1.Fill =  gray;
+                     MinimizeRectangle1.Height = 1.5;
+                }
+            }
+            else
+            {
+                 CloseRectangle1.Fill =  gray;
+                 CloseRectangle2.Fill =  gray;
+                 CloseRectangle1.Height = 1.5;
+                 CloseRectangle2.Height = 1.5;
+            }
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using FontAwesome6;
+using FontAwesome6.Svg;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RETASK
+{
+    internal class WorkWithTasks
+    {
+
+        static List<Dictionary<string, object>> ReadJson()
+        {
+            string text = File.ReadAllText("tasks.json");
+            if (File.Exists("tasks.json") && text.Length >= 2)
+            {
+                List<Dictionary<string, object>> tasks = new List<Dictionary<string, object>>();
+                string json = File.ReadAllText("tasks.json");
+                tasks = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(json);
+                return tasks;
+            }
+            else
+            {
+                List<Dictionary<string, object>> tasks = new List<Dictionary<string, object>>();
+                File.WriteAllText("tasks.json", "[]");
+                string json = File.ReadAllText("tasks.json");
+                tasks = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(json);
+                return tasks;
+            }
+        }
+
+
+        public void updateTasks(WrapPanel wrapPanel)
+        {
+            var tasks = ReadJson();
+            if (!File.Exists("date.txt")) File.WriteAllText("date.txt", DateTime.Today.ToString());
+            else
+            {
+                string content = File.ReadAllText("date.txt");
+                DateTime today = Convert.ToDateTime(content);
+                if (DateTime.Today > today)
+                {
+                    for (int i = 0; i < tasks.Count; i++)
+                    {
+                        Dictionary<string, object> task = tasks[i];
+                        task["complete"] = false;
+                        string updatedjson = JsonConvert.SerializeObject(tasks);
+                        File.WriteAllText("tasks.json", updatedjson);
+                    }
+                    File.WriteAllText("date.txt", DateTime.Today.ToString());
+                    LoadTasks(wrapPanel, "Create");
+                }
+                else if (DateTime.Today == today) LoadTasks(wrapPanel, "Create");
+            }
+        }
+
+        public void AddTask(WrapPanel wrapPanel, string name, Dictionary<string, bool> days, string selectedcolor)
+        {
+            var tasks = ReadJson();
+            TaskUICreate(wrapPanel, name, days, selectedcolor, "Create");
+            Dictionary<string, object> taskData = new Dictionary<string, object>
+            {
+                { "name", name },
+                { "days", days },
+                { "selectedcolor", selectedcolor },
+                { "complete", false }
+            };
+            tasks.Add(taskData);
+            string json = JsonConvert.SerializeObject(tasks);
+            File.WriteAllText("tasks.json", json);
+        }
+        public void DoneTask(object sender, MouseButtonEventArgs e)
+        {
+            var tasks = ReadJson();
+            Canvas canvas = (sender as Rectangle).Parent as Canvas;
+            canvas.Visibility = Visibility.Collapsed;
+            Canvas clickedObject = canvas;
+            WrapPanel wrapPanel = clickedObject.Parent as WrapPanel;
+            int index = wrapPanel.Children.IndexOf(clickedObject);
+            Dictionary<string, object> task = tasks[index];
+            task["complete"] = true;
+            string updatedjson = JsonConvert.SerializeObject(tasks);
+            File.WriteAllText("tasks.json", updatedjson);
+        }
+
+        public void DeleteTask(Canvas clickedObject, int index)
+        {
+            string json = File.ReadAllText("tasks.json");
+            List<Dictionary<string, object>> items = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(json);
+            if (items.Count > 0) items.RemoveAt(index);
+            string updatedJson = JsonConvert.SerializeObject(items);
+            File.WriteAllText("tasks.json", updatedJson);
+            WrapPanel wrapPanel = clickedObject.Parent as WrapPanel;
+            wrapPanel.Children.Remove(clickedObject);
+        }
+
+        private void DoneButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill = (SolidColorBrush)new BrushConverter().ConvertFrom("#00FFFFFF");
+        }
+
+        private void DoneButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill = (SolidColorBrush)new BrushConverter().ConvertFrom("#7eb4ea");
+        }
+
+        private void TaskUICreate(WrapPanel wrapPanel, string name, Dictionary<string, bool> days, string selectedcolor, string operation)
+        {
+            Rectangle donerectangle = new Rectangle();
+            donerectangle.Height = 33.0;
+            donerectangle.Width = 33.0;
+            Canvas.SetLeft(donerectangle, 248.0);
+            Canvas.SetTop(donerectangle, 3.0);
+            donerectangle.RadiusX = 360.0;
+            donerectangle.RadiusY = 360.0;
+            donerectangle.Fill = Brushes.Transparent;
+            donerectangle.MouseDown += DoneTask;
+            donerectangle.MouseEnter += DoneButton_MouseEnter;
+            donerectangle.MouseLeave += DoneButton_MouseLeave;
+            SvgAwesome svgAwesome = new SvgAwesome();
+            svgAwesome.Icon = EFontAwesomeIcon.Solid_CircleCheck;
+            svgAwesome.PrimaryColor = Brushes.WhiteSmoke;
+            svgAwesome.IsHitTestVisible = false;
+            svgAwesome.Height = 29.0;
+            svgAwesome.Width = 29.0;
+            Canvas.SetRight(svgAwesome, 5.0);
+            Canvas.SetTop(svgAwesome, 5.0);
+            svgAwesome.HorizontalAlignment = HorizontalAlignment.Left;
+            svgAwesome.VerticalAlignment = VerticalAlignment.Center;
+            Label label = new Label();
+            label.Content = name;
+            Canvas.SetLeft(label, 10.0);
+            Canvas.SetTop(label, 67.0);
+            label.FontSize = 18.0;
+            label.Foreground = Brushes.White;
+            label.HorizontalAlignment = HorizontalAlignment.Left;
+            label.VerticalAlignment = VerticalAlignment.Center;
+            label.Width = 264.0;
+            label.HorizontalContentAlignment = HorizontalAlignment.Center;
+            label.IsHitTestVisible = false;
+            Label label2 = new Label();
+            foreach (KeyValuePair<string, bool> day in days)
+            {
+                bool value = day.Value;
+                if (value)
+                {
+                    Label label3 = label2;
+                    object content = label3.Content;
+                    label3.Content = ((content != null) ? content.ToString() : null) + " " + day.Key;
+                }
+            }
+            Canvas.SetLeft(label2, 10.0);
+            Canvas.SetTop(label2, 132.0);
+            label2.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFE4E4E4");
+            label2.HorizontalAlignment = HorizontalAlignment.Left;
+            label2.VerticalAlignment = VerticalAlignment.Center;
+            label2.Width = 264.0;
+            label2.IsHitTestVisible = false;
+            Canvas canvas = new Canvas();
+            canvas.Name = "T1";
+            canvas.Height = 168.0;
+            canvas.Width = 284.0;
+            canvas.HorizontalAlignment = HorizontalAlignment.Left;
+            canvas.VerticalAlignment = VerticalAlignment.Top;
+            canvas.Background = (SolidColorBrush)new BrushConverter().ConvertFrom(selectedcolor);
+            canvas.Margin = new Thickness(32.0, 10.0, 0.0, 0.0);
+            if (operation == "Create")
+            {
+                canvas.Children.Add(donerectangle);
+                canvas.Children.Add(svgAwesome);
+            }
+            canvas.Children.Add(label);
+            canvas.Children.Add(label2);
+            foreach (Canvas x in new List<Canvas>{ canvas })
+            {
+                wrapPanel.Children.Add(x);
+            }
+        }
+        public void LoadTasks(WrapPanel wrapPanel, string operation)
+        {
+            var tasks = ReadJson();
+            wrapPanel.Children.Clear();
+            string todayStr = DateTime.Today.DayOfWeek.ToString().Substring(0, 3);
+            if (tasks.Count > 0)
+            {
+                for (int i = 0; i < tasks.Count; i++)
+                {
+                    string selectedcolor = (string)tasks[i]["selectedcolor"];
+                    string name = (string)tasks[i]["name"];
+                    JObject days = tasks[i]["days"] as JObject;
+                    Dictionary<string, bool> days_object = days.ToObject<Dictionary<string, bool>>();
+                    //Dictionary<string, bool> days = (Dictionary<string, bool>)tasks[i]["days"];
+                    TaskUICreate(wrapPanel, name, days_object, selectedcolor, operation);
+                }
+            }
+        }
+    }
+}
